a41d09479f2c0e5bffb67201d4bb7c86038fbe52
yasuhiro.umei <yasuhiroumei@yahoo.co.jp>
2021-08-24 15:52:54 +0900

    再修正

--- src/models/Task.java
+++ src/models/Task.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
         query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
     )
 })
-@Table(name = "tasklist")
+@Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
